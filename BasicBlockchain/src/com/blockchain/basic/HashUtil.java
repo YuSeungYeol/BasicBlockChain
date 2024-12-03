8c4e9b2088dec5947195a0c9be703b657d5b983b
YuSeungYeol <sr.0220a@gmail.com>
2024-12-03 13:47:21 +0900

    [유승열] hash 수정

--- BasicBlockchain/src/com/blockchain/basic/HashUtil.java
+++ BasicBlockchain/src/com/blockchain/basic/HashUtil.java
@@ -13,7 +13,7 @@ public class HashUtil {
                 String hex = Integer.toHexString(0xff & b);
                 if (hex.length() == 1) hexString.append('0');
                 hexString.append(hex);
-            }
+            } 
             return hexString.toString();
         } catch (Exception e) {
             throw new RuntimeException(e);
