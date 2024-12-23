60791557c128569035b26c1c02c00e16a5363bd6
YuSeungYeol <sr.0220a@gmail.com>
2024-12-23 10:36:00 +0900

    [유승열] 외부 데이터 추가

---
+++ BasicBlockchain/src/com/blockchain/basic/ExternalDataFetcher.java
@@ -0,0 +1,13 @@
+package com.blockchain.basic;
+
+import java.util.Random;
+
+public class ExternalDataFetcher {
+
+	public static double fetchExchangeRate() {
+        // 실제로는 외부 API 호출을 통해 데이터를 가져와야 함
+        // 예: https://api.exchangerate-api.com/v4/latest/USD
+        Random random = new Random();
+        return 950 + (random.nextDouble() * 200); // 950 ~ 1150 사이의 임의 환율
+    }
+}
