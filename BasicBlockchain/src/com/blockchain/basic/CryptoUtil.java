463efe66b97918ea5dc4dea87c3c8a30c9254917
YuSeungYeol <sr.0220a@gmail.com>
2025-01-08 10:26:26 +0900

    [유승열] 암호화 및 복호화 수정

---
+++ BasicBlockchain/src/com/blockchain/basic/CryptoUtil.java
@@ -0,0 +1,36 @@
+package com.blockchain.basic;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class CryptoUtil {
+    private static final String ALGORITHM = "AES";
+
+    // 키 생성
+    public static SecretKey generateKey() throws Exception {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM);
+        keyGenerator.init(128);
+        return keyGenerator.generateKey();
+    }
+
+    // 암호화
+    public static String encrypt(String data, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedData);
+    }
+
+    // 복호화
+    public static String decrypt(String encryptedData, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decodedData = Base64.getDecoder().decode(encryptedData);
+        byte[] originalData = cipher.doFinal(decodedData);
+        return new String(originalData);
+    }
+}
+
